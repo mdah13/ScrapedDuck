4f13875b140dd2d256ee3aeca0a2b1cc1ca4f79f
professorbinocs <45925155+professorbinocs@users.noreply.github.com>
2022-06-29 21:14:29 -0700

    Multiple spotlight mons

--- pages/detailed/spotlight.js
+++ pages/detailed/spotlight.js
@@ -3,61 +3,66 @@ const jsd = require('jsdom');
 const { JSDOM } = jsd;
 const https = require('https');
 
-function get(url, id, bkp)
-{
+function get(url, id, bkp) {
     return new Promise(resolve => {
         JSDOM.fromURL(url, {
         })
-        .then((dom) => {
+            .then((dom) => {
 
-            var content = dom.window.document.querySelectorAll('.pkmn-list-flex')[0];
+                const content = dom.window.document.querySelectorAll('.pkmn-list-flex')[0];
 
-            var spotlight = {
-                name: "",
-                canBeShiny: false,
-                image: "",
-                bonus: [],
-                graphic: ""
-            };
+                var event = {
+                    name: "",
+                    canBeShiny: false,
+                    image: "",
+                    bonus: [],
+                    graphic: ""
+                };
 
-            const images = dom.window.document.querySelectorAll('img');
+                const images = dom.window.document.querySelectorAll('img');
 
-            for (let i = 0; i < images.length; i++) {
-                const img = images[i];
-                if (img.alt === "Graphic") {
-                    spotlight.graphic = img.src
+                for (let i = 0; i < images.length; i++) {
+                    const img = images[i];
+                    if (img.alt === "Graphic") {
+                        event.graphic = img.src
+                        break
+                    }
                 }
-            }
 
-            spotlight.name = content.querySelector(":scope > .pkmn-list-item > .pkmn-name").innerHTML;
-            spotlight.canBeShiny = content.querySelector(":scope > .pkmn-list-item > .shiny-icon") != null;
-            spotlight.image = content.querySelector(":scope > .pkmn-list-item > .pkmn-list-img > img").src;
+                pokemons = content.querySelectorAll(".pkmn-name");
 
-            var temp = dom.window.document.querySelectorAll('.event-description')[0].innerHTML;
-            var split = temp.split("<strong>");
-            spotlight.bonus.push(split[split.length - 1].split("</strong>")[0]);
+                names = []
+                for (let i = 0; i < pokemons.length; i++) {
+                    const pokeName = pokemons[i];
+                    names.push(pokeName.innerHTML)
+                }
 
-            fs.writeFile(`files/temp/${id}.json`, JSON.stringify({ id: id, type: "pokemon-spotlight-hour", data: spotlight }), err => {
-                if (err) {
-                    console.error(err);
-                    return;
+                event.name = names.join(', ')
+                event.canBeShiny = content.querySelector(":scope > .pkmn-list-item > .shiny-icon") != null;
+                event.image = content.querySelector(":scope > .pkmn-list-item > .pkmn-list-img > img").src;
+
+                var temp = dom.window.document.querySelectorAll('.event-description')[0].innerHTML;
+                var split = temp.split("<strong>");
+                event.bonus.push(split[split.length - 1].split("</strong>")[0]);
+
+                fs.writeFile(`files/temp/${id}.json`, JSON.stringify({ id: id, type: "pokemon-spotlight-hour", data: event }), err => {
+                    if (err) {
+                        console.error(err);
+                        return;
+                    }
+                });
+            }).catch(_err => {
+                for (var i = 0; i < bkp.length; i++) {
+                    if (bkp[i].eventID == id) {
+                        fs.writeFile(`files/temp/${id}.json`, JSON.stringify({ id: id, type: "pokemon-spotlight-hour", data: bkp[i].extraData }), err => {
+                            if (err) {
+                                console.error(err);
+                                return;
+                            }
+                        });
+                    }
                 }
             });
-        }).catch(_err =>
-        {
-            for (var i = 0; i < bkp.length; i++)
-            {
-                if (bkp[i].eventID == id)
-                {
-                    fs.writeFile(`files/temp/${id}.json`, JSON.stringify({ id: id, type: "pokemon-spotlight-hour", data: bkp[i].extraData }), err => {
-                        if (err) {
-                            console.error(err); 
-                            return;
-                        }
-                    });
-                }
-            }
-        });
     })
 }
 
