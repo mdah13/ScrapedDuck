d764b3fd6e4f8cf96080be8c051d36dccbbc4977
professorbinocs <45925155+professorbinocs@users.noreply.github.com>
2022-06-29 21:14:00 -0700

    Multiple breakthrough mons

--- pages/detailed/breakthrough.js
+++ pages/detailed/breakthrough.js
@@ -12,17 +12,26 @@ function get(url, id, bkp)
 
             var content = dom.window.document.querySelectorAll('.pkmn-list-flex')[0];
 
-            var reward = {
+            var event = {
                 name: "",
                 canBeShiny: false,
                 image: ""
             };
 
-            reward.name = content.querySelector(":scope > .pkmn-list-item > .pkmn-name").innerHTML;
-            reward.canBeShiny = content.querySelector(":scope > .pkmn-list-item > .shiny-icon") != null;
-            reward.image = content.querySelector(":scope > .pkmn-list-item > .pkmn-list-img > img").src;
+            pokemons = content.querySelectorAll(".pkmn-name");
 
-            fs.writeFile(`files/temp/${id}.json`, JSON.stringify({ id: id, type: "research-breakthrough", data: reward }), err => {
+            names = []
+            for (let i = 0; i < pokemons.length; i++) {
+                const pokeName = pokemons[i];
+                names.push(pokeName.innerHTML)
+            }
+
+            event.name = names.join(', ')
+
+            event.canBeShiny = content.querySelector(":scope > .pkmn-list-item > .shiny-icon") != null;
+            event.image = content.querySelector(":scope > .pkmn-list-item > .pkmn-list-img > img").src;
+
+            fs.writeFile(`files/temp/${id}.json`, JSON.stringify({ id: id, type: "research-breakthrough", data: event }), err => {
                 if (err) {
                     console.error(err);
                     return;
